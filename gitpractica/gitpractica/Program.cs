5a062a0110bf6f6400617bbdcfa26e920b285b67
Adelpozo04 <125442786+Adelpozo04@users.noreply.github.com>
2023-03-09 12:35:57 +0100

    Vaya basura

---
+++ gitpractica/gitpractica/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gitpractica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
